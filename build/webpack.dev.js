94ac05ffb8e5e6aeb79bbd33088f71d486448795
daniel <daniel@192.168.124.14>
2019-10-07 23:13:53 +0800

    hashrouter/browserrouter

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: path.resolve(__dirname, "../src/index.js"),
   output: {
     path: path.resolve(__dirname, "../dist"),
@@ -17,10 +16,14 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "../dist"),
+    compress: true,
+    port: 4000
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "../public/index.html")
-    }),
-    new CleanWebpackPlugin()
+    })
   ]
 };
--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import Login from "./Login";
 
 const App = () => {
-  return <div>123</div>;
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/login' exact component={Login} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
